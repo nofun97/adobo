ae7999ff51588b6dcccaa65aac647b2ae0b25e5a
Novan <25704935+nofun97@users.noreply.github.com>
2022-05-12 11:18:55 +1000

    allow any regex string in patternProperties

--- testdata/test.json.go
+++ testdata/test.json.go
@@ -25,6 +25,7 @@ type TestObj struct {
     EmptyObject2 struct{} `json:"emptyObject2,omitempty" yaml:"emptyObject2,omitempty"`
     JustString string `json:"justString,omitempty" yaml:"justString,omitempty"`
     MapObject map[string]*TestObjMapObjectValue `json:"mapObject,omitempty" yaml:"mapObject,omitempty"`
+    MapObjectWithDifferentPattern map[string]*TestObjMapObjectWithDifferentPatternValue `json:"mapObjectWithDifferentPattern,omitempty" yaml:"mapObjectWithDifferentPattern,omitempty"`
     MapOfEmptyObject map[string]struct{} `json:"mapOfEmptyObject,omitempty" yaml:"mapOfEmptyObject,omitempty"`
     ObjectWithField []*TestObjObjectWithFieldItem `json:"objectWithField,omitempty" yaml:"objectWithField,omitempty"`
 }
@@ -32,6 +33,10 @@ type TestObj struct {
 type TestObjMapObjectValue struct {
     Prop1 string `json:"prop1,omitempty" yaml:"prop1,omitempty"`
     Prop2 []string `json:"prop2,omitempty" yaml:"prop2,omitempty"`
+}
+
+type TestObjMapObjectWithDifferentPatternValue struct {
+    Prop1 float `json:"prop1,omitempty" yaml:"prop1,omitempty"`
 }
 
 type TestObjObjectWithFieldItem struct {
